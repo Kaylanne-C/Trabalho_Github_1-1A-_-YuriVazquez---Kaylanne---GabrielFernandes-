5c9003894d0909406e4ccf1449ced090552de0ac
YuriVazquez <yurichapeta10@gmail.com>
2019-11-26 10:01:47 -0200

    Implementei as duas funções, além disso coloquei o arquivo.

---
+++ projetoGIT1 (1).cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+
+/*
+	Conta todas as letras 'letra' contidas em 'texto'
+*/
+int contaLetras(char letra, string texto)
+{
+}
+
+/*
+	Substitui todas as ocorrencias de 'letra1' por 'letra2' contidas em 'texto'
+*/
+void substituirLetra(char letra1,char letra2, string texto)
+{
+	for(i=0;i<texto.lenght();i++)(
+	if(letra1==texto[i]=letra2;
+}
+
+/*
+	Conta todas as ocorrencias de 'palavra' contidas em 'texto'
+*/
+int contaPalavras(string palavra, string texto)
+{
+	
+	int i,j=0;
+	cout<<"insira o texto:";
+	getline(cin, texto);
+	cout<<"insira a letra a ser procurada:";
+	cin>> letra;
+	for(i=0;i<texto, lenght(), i++){
+	if(texto[i]==letra){
+	j=j+1;
+	return j;
+	
+}
+
+/*
+	Substitui todas as ocorrencias de 'palavra1' por 'palavra2' em 'texto'
+*/
+void substituirPalavras(string palavra1, string palavra2, string texto)
+{
+}
+
+int main(){
+
+	cout << "Entre com o texto para fazer o teste:";
+
+
+
+
+	return 0;
+}
